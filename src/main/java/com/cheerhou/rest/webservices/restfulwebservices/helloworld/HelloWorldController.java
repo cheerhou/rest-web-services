c51c741dcddeedc250aa83232cb8ad150a8146b8
Chenjin Hou <houchenjin@23mofang.com>
2020-12-10 13:56:17 +0800

    internationalization

--- src/main/java/com/cheerhou/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/cheerhou/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,11 @@
 package com.cheerhou.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 /**
  * @author hcj
@@ -10,6 +15,9 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
+    @Autowired
+    private MessageSource messageSource;
+
     //NOTES: two annotations are the same
 //    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     @GetMapping("/hello-world")
@@ -25,5 +33,10 @@ public class HelloWorldController {
     @GetMapping("/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldBean(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping("/hello-world/internationalized")
+    public String helloWorldBeanInternationalized() {
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 }
